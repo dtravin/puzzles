9918b61299d455ea3df6c72148c05b4061d8559d
Daniel Travin <dtravin@gmail.com>
2018-11-04 22:57:28 +0200

    Onlog solution

--- highest_product_of_3/solution.py
+++ highest_product_of_3/solution.py
@@ -1,19 +1,3 @@
 def solution(a):
-    high = max(a[0], a[1])
-    low = min(a[0], a[1])
-
-    hp2_high = a[0] * a[1]
-    hp2_low = a[0] * a[1]
-
-    hp3 = -10E8
-
-    for e in a[2:]:
-        hp3 = max(hp3, hp2_high * e, hp2_low * e)
-
-        hp2_low = min(hp2_low, low * e, high * e)
-        hp2_high = max(hp2_high, low * e, high * e)
-
-        low = min(low, e)
-        high = max(high, e)
-
-    return hp3
+    a.sort()
+    return max(a[-1] * a[-2] * a[-3], a[-1] * a[0] * a[1])
--- highest_product_of_3/solution_test.py
+++ highest_product_of_3/solution_test.py
@@ -5,7 +5,8 @@ from solution import solution
 class TestHighestProductOf3(unittest.TestCase):
 
     def test_basic(self):
-        self.assertEqual(300, solution([-100, -1, 0, 1, 2, 3]))
+        self.assertEqual(60, solution([1, 2, 3, 4, 5, 1]))
+        self.assertEqual(150, solution([-10, -5, 0, 1, 2, 3]))
 
 if __name__ == '__main__':
     unittest.main()
